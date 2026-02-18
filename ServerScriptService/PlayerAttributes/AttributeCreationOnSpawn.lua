40bda6329d3a728c1cecacdd7d21755e5c8dc06d
aidan c. <ahcollis2@gmail.com>
2026-02-18 18:18:40 -0500

    AttributeCreationOnSpawn.lua Creation / Base
    Creation of the base state of the AttributeCreationOnSpawn.lua file. This file features variables assigned to each individual player when they spawn/join. The file will likely be updated in the future to allow for more variables.

---
+++ ServerScriptService/PlayerAttributes/AttributeCreationOnSpawn.lua
@@ -0,0 +1,31 @@
+game.Players.PlayerAdded:Connect(function(player)
+	
+	local playerVar = Instance.new("Folder", player)
+	playerVar.Name = "PlayerAttributes"
+	
+	--[[
+	These are default values upon joining in game. Many of these attributes
+	will be overwritten by most character archetypes upon joining a round.
+	]]--
+	
+	local inRound = Instance.new("BoolValue", playerVar)
+	inRound.Name = "InRound"
+	inRound.Value = false
+	
+	local hp = Instance.new("IntValue", playerVar)
+	hp.Name = "Health"
+	hp.Value = 100
+	
+	local maxHp = Instance.new("IntValue", playerVar)
+	maxHp.Name = "MaxHealth"
+	maxHp.Value = 100
+	
+	local stm = Instance.new("IntValue", playerVar)
+	stm.Name = "Stamina"
+	stm.Value = 100
+	
+	local maxStm = Instance.new("IntValue", playerVar)
+	maxStm.Name = "MaxStamina"
+	maxStm.Value = 100
+	
+end)
