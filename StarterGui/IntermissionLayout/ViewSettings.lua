368dcd6895ea8482176c7f224199b8722aa7cda0
aidan c. <ahcollis2@gmail.com>
2026-03-18 17:45:58 -0400

    ViewSettings.lua Button Inclusion
    Added buttons to the settings tab to give it proper usage.

--- StarterGui/IntermissionLayout/ViewSettings.lua
+++ StarterGui/IntermissionLayout/ViewSettings.lua
@@ -1,6 +1,8 @@
 local gameSettings = script.Parent
 local settingsMenu = gameSettings.SettingsMenu
 local settingsButton = gameSettings.SettingsButton
+local settingsTexts = settingsMenu.SettingsTexts
+local settingsButtons = settingsMenu.SettingsButtons
 
 local replicatedStorage = game.ReplicatedStorage
 local intermission = replicatedStorage.TimerFires.BeginIntermission
@@ -10,6 +12,16 @@ local idTab = "Settings"
 local guiFunctions = require(replicatedStorage.ModuleScripts.GuiFunctions)
 
 local player = game.Players.LocalPlayer
+
+local maliceGain = player.PlayerAttributes.MaliceGain
+local afkMode = player.PlayerAttributes.AfkMode
+local hitboxesEnabled = player.PlayerAttributes.HitboxesEnabled
+
+local maliceButton = settingsButtons.MaliceCheck
+local afkButton = settingsButtons.AfkCheck
+local hitboxesButton = settingsButtons.HitboxesCheck
+
+--local bass = settingsMenu.Bass
 
 settingsMenu.Visible = false
 settingsButton.Visible = true
@@ -28,5 +40,35 @@ settingsButton.MouseButton1Click:Connect(function()
 		guiFunctions.closeTabs(idTab)
 	else
 		guiFunctions.closeTabs(nil)
+	end
+end)
+
+maliceButton.MouseButton1Click:Connect(function()
+	if maliceGain.Value == true then
+		maliceGain.Value = false
+		maliceButton.Image = "rbxassetid://82700746510321"
+	else
+		maliceGain.Value = true
+		maliceButton.Image = "rbxassetid://98354154174040"
+	end
+end)
+
+afkButton.MouseButton1Click:Connect(function()
+	if afkMode.Value == false then
+		afkMode.Value = true
+		afkButton.Image = "rbxassetid://82700746510321"
+	else
+		afkMode.Value = false
+		afkButton.Image = "rbxassetid://98354154174040"
+	end
+end)
+
+hitboxesButton.MouseButton1Click:Connect(function()
+	if hitboxesEnabled.Value == false then
+		hitboxesEnabled.Value = true
+		hitboxesButton.Image = "rbxassetid://82700746510321"
+	else
+		hitboxesEnabled.Value = false
+		hitboxesButton.Image = "rbxassetid://98354154174040"
 	end
 end)
