bf726d216d1ca3744d02a792efb1edc538e22d45
aidan c. <ahcollis2@gmail.com>
2026-03-27 09:44:55 -0400

    Timers.lua Team Inclusion
    Added a block of code to Timers.lua to make the highest malice person in the server the Juggernaut upon starting the round, while making all other players Survivors.

--- ServerScriptService/RoundScripts/Timers.lua
+++ ServerScriptService/RoundScripts/Timers.lua
@@ -4,13 +4,20 @@ local roundInProgress = replicatedStorage.ServerVariables.RoundInProgress
 
 local intermission = replicatedStorage.TimerFires.BeginIntermission
 local round = replicatedStorage.TimerFires.BeginRound
+local pickTeams = replicatedStorage.TimerFires.PickTeams
+
+local player = game.Players.LocalPlayer
+local players = game.Players:GetPlayers()
+local team = ""
+local malice = 0
+local highestMalice = 0
+local nextJuggernaut = 0
 
 local function roundClock(startingTime)
 	time.Value = startingTime
 	while time.Value > 0 do
 		time.Value -= 1
 		task.wait(1)
-		print(time.Value)
 	end
 end
 
@@ -19,6 +26,25 @@ while true do
 	roundInProgress.Value = false
 	roundClock(15) -- short for now for testing purposes, will be extended to 45 seconds before final version
 	round:FireAllClients()
+	
+	players = game.Players:GetPlayers()
+	for _, player in ipairs(players) do
+		if player.PlayerAttributes.AfkMode.Value == true then
+			continue
+		end
+		player.PlayerAttributes.Team.Value = "Survivor"
+		malice = player.leaderstats.Malice.Value
+		if malice >= highestMalice then
+			highestMalice = malice
+			nextJuggernaut = player
+			nextJuggernautName = player.Name
+		end
+	end
+	nextJuggernaut.PlayerAttributes.Team.Value = "Juggernaut"
+	nextJuggernaut.PlayerAttributes.Malice.Value = 0
+	pickTeams:FireAllClients()
+	print(nextJuggernautName .. " is the next juggernaut")
+	
 	roundInProgress.Value = true
 	task.wait(1)
 	roundClock(210)
