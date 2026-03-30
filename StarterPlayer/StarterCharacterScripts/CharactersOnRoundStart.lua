e4ca9427fc590cf2aa941e35d25493221bacfc98
aidan c. <ahcollis2@gmail.com>
2026-03-30 19:10:53 -0400

    CharactersOnRoundStart.lua Revamp
    Teleports you to a specific spawn point on the map based on your role, properly applies stats, and no longer teleports those marked as AFK.

--- StarterPlayer/StarterCharacterScripts/CharactersOnRoundStart.lua
+++ StarterPlayer/StarterCharacterScripts/CharactersOnRoundStart.lua
@@ -2,13 +2,44 @@ local replicatedStorage = game.ReplicatedStorage
 local pickTeams = replicatedStorage.TimerFires.PickTeams
 local player = game.Players.LocalPlayer
 local team = player.PlayerAttributes.Team
-local humanoid = player.Character.Humanoid
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+local stamina = player.PlayerAttributes.Stamina
+local health = player.PlayerAttributes.Health
+local maxStamina = player.PlayerAttributes.MaxStamina
+local maxHealth = player.PlayerAttributes.MaxHealth
+local walkSpeed = player.PlayerAttributes.WalkSpeed
+local runSpeed = player.PlayerAttributes.RunSpeed
+local inRound = player.PlayerAttributes.InRound
+
+local survivorSpawn = game.Workspace:WaitForChild("SurvivorSpawn")
+local juggernautSpawn = game.Workspace:WaitForChild("JuggernautSpawn")
 
 pickTeams.OnClientEvent:Connect(function()
-	if team.Value == "Survivor" then
-		--humanoid.RootPart.Position = Vector3.new(0, 300, 0)
-	elseif team.Value == "Juggernaut" then
-		--humanoid.RootPart.Position = Vector3.new(0, 600, 300)
+	if inRound.Value == true then
+		if team.Value == "Survivor" then
+			character:MoveTo(survivorSpawn.Position + Vector3.new(math.random(-3, 3), 5, math.random(-3, 3)))
+
+			stamina.Value = 100
+			health.Value = 100
+			maxStamina.Value = 100
+			maxHealth.Value = 100
+			walkSpeed.Value = 12
+			runSpeed.Value = 29
+
+		elseif team.Value == "Juggernaut" then
+			character:MoveTo(juggernautSpawn.Position + Vector3.new(math.random(-3, 3), 5, math.random(-3, 3)))
+			
+			stamina.Value = 120
+			health.Value = 900
+			maxStamina.Value = 120
+			maxHealth.Value = 900
+			walkSpeed.Value = 6
+			runSpeed.Value = 32
+			
+		end
+		humanoid.WalkSpeed = walkSpeed.Value
+		print(team.Value)
 	end
-	print(team.Value)
 end)
