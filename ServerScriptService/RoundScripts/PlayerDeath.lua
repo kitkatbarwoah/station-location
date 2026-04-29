9ad8eabd89ce302e53218ef42c346ec30edd547a
aidan c. <ahcollis2@gmail.com>
2026-04-29 10:16:42 -0400

    PlayerDeath.lua Quota Implementation
    Edited player deaths to change the quota based on whether a player is in chase with a Juggernaut.

--- ServerScriptService/RoundScripts/PlayerDeath.lua
+++ ServerScriptService/RoundScripts/PlayerDeath.lua
@@ -2,6 +2,12 @@ local replicatedStorage = game.ReplicatedStorage
 local playerDeath = replicatedStorage.CharacterFires.PlayerDeath
 local increaseTime = replicatedStorage.TimerFires.IncreaseTime
 local prematurelyEndGame = replicatedStorage.TimerFires.PrematurelyEndGame
+local player = game.Players.LocalPlayer
+local quota = replicatedStorage.ServerVariables.Quota
+local quotaProgress = replicatedStorage.ServerVariables.QuotaProgress
+
+local charPosition = player.PlayerAttributes.CharPosition
+local juggernautPosition = game.ReplicatedStorage.ServerVariables.JuggernautPosition
 
 playerDeath.OnServerEvent:Connect(function(player)
 	local inRound = player.PlayerAttributes.InRound
@@ -9,7 +15,12 @@ playerDeath.OnServerEvent:Connect(function(player)
 	
 	inRound.Value = false
 	if team.Value == "Survivor" then
-		increaseTime:Fire(35)
+		if (charPosition.Value - juggernautPosition.Value).Magnitude < 80 then
+			quotaProgress += 1
+			increaseTime:Fire(35)
+		else
+			quota *= math.round(((3/2) - 1) * 2/3)
+		end
 	elseif team.Value == "Juggernaut" then
 		prematurelyEndGame:Fire()
 	end
