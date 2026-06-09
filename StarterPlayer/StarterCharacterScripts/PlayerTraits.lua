b997e3ba06b38d9d141683e9c24ca0bc3f0b9d87
aidan c. <ahcollis2@gmail.com>
2026-06-08 22:25:42 -0400

    PlayerTraits.lua Speed Update
    Players now receive direct speed changes upon interacting with certain abilities (WIP).

--- StarterPlayer/StarterCharacterScripts/PlayerTraits.lua
+++ StarterPlayer/StarterCharacterScripts/PlayerTraits.lua
@@ -30,6 +30,7 @@ local replicatedStorage = game.ReplicatedStorage
 local intermission = replicatedStorage.TimerFires.BeginIntermission
 local round = replicatedStorage.TimerFires.BeginRound
 local updateSpeed = replicatedStorage.CharacterFires.UpdateSpeed
+local requestSpeedChange = replicatedStorage.CharacterFires.RequestSpeedChange
 local playerDeath = replicatedStorage.CharacterFires.PlayerDeath
 local chaseProximity = replicatedStorage.LocalFires.ChaseProximity
 
@@ -49,7 +50,24 @@ end)
 
 updateSpeed.OnClientEvent:Connect(function(plr)
 	if player == plr then
-		print(abilitySpeedMult.Value)
+		if sprinting == true then
+			humanoid.WalkSpeed = runSpeed.Value * speedMult.Value * abilitySpeedMult.Value
+		elseif sprinting == false then
+			humanoid.WalkSpeed = walkSpeed.Value * speedMult.Value * abilitySpeedMult.Value
+		end
+	end
+end)
+
+requestSpeedChange.OnClientEvent:Connect(function(plr, boost, duration)
+	if player == plr then
+		speedMult.Value += boost
+		if sprinting == true then
+			humanoid.WalkSpeed = runSpeed.Value * speedMult.Value * abilitySpeedMult.Value
+		elseif sprinting == false then
+			humanoid.WalkSpeed = walkSpeed.Value * speedMult.Value * abilitySpeedMult.Value
+		end
+		task.wait(duration)
+		speedMult.Value -= boost
 		if sprinting == true then
 			humanoid.WalkSpeed = runSpeed.Value * speedMult.Value * abilitySpeedMult.Value
 		elseif sprinting == false then
