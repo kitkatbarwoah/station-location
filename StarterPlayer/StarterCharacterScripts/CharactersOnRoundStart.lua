afe54ce1f62676dd4fb03852214052eb5a0f9391
aidan c. <ahcollis2@gmail.com>
2026-06-08 22:22:50 -0400

    CharactersOnRoundStart.lua Survivor Stats
    Grants survivors different base stats (health, stamina, speed, cooldowns) upon joining a round based on their class.

--- StarterPlayer/StarterCharacterScripts/CharactersOnRoundStart.lua
+++ StarterPlayer/StarterCharacterScripts/CharactersOnRoundStart.lua
@@ -18,6 +18,9 @@ local cooldown2max = player.PlayerAttributes.Cooldown2Max
 local cooldown3max = player.PlayerAttributes.Cooldown3Max
 local cooldown4max = player.PlayerAttributes.Cooldown4Max
 
+local equippedSurvivor = player.PlayerAttributes.EquippedCharacterName
+local equippedJuggernaut = player.PlayerAttributes.EquippedJuggernautName
+
 local survivorSpawn = game.Workspace:WaitForChild("SurvivorSpawn")
 local juggernautSpawn = game.Workspace:WaitForChild("JuggernautSpawn")
 
@@ -25,19 +28,27 @@ pickTeams.OnClientEvent:Connect(function()
 	if inRound.Value == true then
 		if team.Value == "Survivor" then
 			character:MoveTo(survivorSpawn.Position + Vector3.new(math.random(-3, 3), 5, math.random(-3, 3)))
-
-			stamina.Value = 100
-			health.Value = 100
-			maxStamina.Value = 100
-			maxHealth.Value = 100
-			walkSpeed.Value = 12
-			runSpeed.Value = 29
-			cooldown2max.Value = 40
-			cooldown3max.Value = 35
-
+			if equippedSurvivor.Value == "Archer" then
+				stamina.Value = 70
+				health.Value = 90
+				maxStamina.Value = 70
+				maxHealth.Value = 90
+				walkSpeed.Value = 13
+				runSpeed.Value = 32
+				cooldown2max.Value = 20
+				cooldown3max.Value = 24
+			elseif equippedSurvivor.Value == "Doctor" then
+				stamina.Value = 100
+				health.Value = 80
+				maxStamina.Value = 100
+				maxHealth.Value = 80
+				walkSpeed.Value = 12
+				runSpeed.Value = 29
+				cooldown2max.Value = 36
+				cooldown3max.Value = 16
+			end
 		elseif team.Value == "Juggernaut" then
 			character:MoveTo(juggernautSpawn.Position + Vector3.new(math.random(-3, 3), 5, math.random(-3, 3)))
-			
 			stamina.Value = 120
 			health.Value = 900
 			maxStamina.Value = 120
@@ -48,7 +59,6 @@ pickTeams.OnClientEvent:Connect(function()
 			cooldown2max.Value = 15
 			cooldown3max.Value = 12
 			cooldown4max.Value = 20
-			
 		end
 		humanoid.WalkSpeed = walkSpeed.Value
 	end
