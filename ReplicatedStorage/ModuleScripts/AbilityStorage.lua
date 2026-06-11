87dd0542baebeb61b089ad25fca79fa0f239651f
aidan c. <ahcollis2@gmail.com>
2026-06-10 22:32:00 -0400

    AbilityStorage.lua Tweaks
    Modifying abilities to make them work better. Additionally, added unique face animations that play upon using certain abilities.

--- ReplicatedStorage/ModuleScripts/AbilityStorage.lua
+++ ReplicatedStorage/ModuleScripts/AbilityStorage.lua
@@ -63,7 +63,7 @@ function module.butcherLeap(player, damage, windup, forwardPower)
 	while humanoid.FloorMaterial == Enum.Material.Air or i < 10 do
 		i += 1
 		local initialDirection = humanoid.RootPart.CFrame.LookVector * Vector3.new(0.4, 0, 0.4)
-		character:PivotTo(character:GetPivot() + initialDirection * forwardPower + Vector3.new(0, (-i+60)/18, 0))
+		character:PivotTo(character:GetPivot() + initialDirection * forwardPower + Vector3.new(0, (-i+60)/18 - (i * i)/300, 0))
 		if forwardPower > 0 then
 			forwardPower -= 0.15
 		end
@@ -87,6 +87,28 @@ function module.butcherLeap(player, damage, windup, forwardPower)
 	end
 	
 	dmgDealt = false
+	
+	--damage effect
+	for j = 1, math.random(7, 15) do
+		local fragmentClone = fragment:Clone()
+		fragmentClone.Parent = workspace
+		local fragmentSize = math.random(1, 8)/8
+		fragmentClone.Size = Vector3.new(fragmentSize, fragmentSize, fragmentSize)
+		fragmentClone.CFrame = humanoid.RootPart.CFrame
+		fragmentClone.CFrame *= CFrame.Angles(0, math.rad(math.random(0, 360)), 0)
+		fragmentClone.CFrame *= CFrame.new(math.random(-8, 8), math.random(-1, 1), math.random(-8, 8))
+		fragment.AssemblyLinearVelocity = Vector3.new(math.random(-15, 15), math.random(25, 65), math.random(-15, 15))
+		fragment.AssemblyAngularVelocity = Vector3.new(math.random(-10, 10), math.random(-10, 10), math.random(-10, 10))
+		task.delay(0.25, function()
+			fragmentClone.CanCollide = true
+			task.wait(1)
+			for k = 1, 30 do
+				fragmentClone.Transparency = (k)/30
+				heartbeat:Wait()
+			end
+			fragmentClone:Destroy()
+		end)
+	end
 	
 	for j = 1, 6 do
 		local hitboxClone = hitbox:Clone()
@@ -165,6 +187,14 @@ function module.butcherPin(player, damage, windup, projectileSpeed, projectileLi
 					contact.Parent:FindFirstChild("Humanoid"):TakeDamage(dmg)
 					dmgDealt = true
 					i = 180
+					
+					if i > 20 then
+						abilitySpeedMult.Value = 1
+						updateSpeed:FireAllClients(player)
+						abilityReady:FireAllClients(player)
+						character.Weapon.Handle.Transparency = 0
+					end
+					
 					visualReduction:FireAllClients(player, 3)
 					cooldownReduction:Fire(player, 3, 8)
 
@@ -204,26 +234,22 @@ function module.archerArrow(player, damage, windup, projectileSpeed, projectileL
 	local walkSpeed = player.PlayerAttributes.WalkSpeed
 	local runSpeed = player.PlayerAttributes.RunSpeed
 	local abilitySpeedMult = player.PlayerAttributes.AbilitySpeedMult
+	local face = character.Head.face
 	local dmg = damage
 	local dmgDealt = false
 	local i = 0	
 
 	abilitySpeedMult.Value = 0.4
 	updateSpeed:FireAllClients(player)
+	face.Texture = "rbxassetid://96866396006967"
 
 	task.wait(windup)
+	face.Texture = "rbxassetid://105322980330006"
 
-	character.Weapon.Handle.Transparency = 1
 	local originalCFrame = humanoid.RootPart.CFrame
 
 	while i < projectileLifespan * 60 do
 		i += 1
-		if i == 30 then
-			abilitySpeedMult.Value = 1
-			updateSpeed:FireAllClients(player)
-			abilityReady:FireAllClients(player)
-
-		end
 		local hitboxClone = hitbox:Clone()
 		local arrowClone = replicatedStorage.Assets["Arrow"]:Clone()
 		arrowClone.Parent = hitboxClone
@@ -239,13 +265,20 @@ function module.archerArrow(player, damage, windup, projectileSpeed, projectileL
 		hitboxClone.Touched:Connect(function(hit)
 			local hitboxContacts = hitboxClone:GetTouchingParts()
 			for _, contact in ipairs(hitboxContacts) do
-				if contact.Parent:FindFirstChild("Humanoid") and contact.Parent:FindFirstChild("Humanoid") ~= character:FindFirstChild("Humanoid") and players:GetPlayerFromCharacter(contact.Parent).PlayerAttributes.Team and dmgDealt == false then
+				if contact.Parent:FindFirstChild("Humanoid") and contact.Parent:FindFirstChild("Humanoid") ~= character:FindFirstChild("Humanoid") and dmgDealt == false then
 					contact.Parent:FindFirstChild("Humanoid"):TakeDamage(dmg)
 					dmgDealt = true
-					i = 180
+					i = 182
 				end
 			end
 		end)
+		if i == 30 or i == 182 then
+			face.Texture = "rbxassetid://83146247547038"
+			abilitySpeedMult.Value = 1
+			updateSpeed:FireAllClients(player)
+			abilityReady:FireAllClients(player)
+
+		end
 		heartbeat:Wait()
 		hitboxClone:Destroy()
 	end
@@ -262,10 +295,10 @@ function module.archerDash(player, forwardPower)
 	abilitySpeedMult.Value = 0.001
 	updateSpeed:FireAllClients(player)
 
-	while i < 44 do
+	while i < 32 do
 		i += 1
 		local initialDirection = humanoid.RootPart.CFrame.LookVector * Vector3.new(0.3, 0, 0.3)
-		character:PivotTo(character:GetPivot() + initialDirection * forwardPower + Vector3.new(0, 0, 0))
+		character:PivotTo(character:GetPivot() + initialDirection * forwardPower)
 		if forwardPower > 0 then
 			forwardPower -= 0.05
 		end
@@ -283,11 +316,12 @@ function module.doctorMedkit(player, heal, linger, windup)
 	local walkSpeed = player.PlayerAttributes.WalkSpeed
 	local runSpeed = player.PlayerAttributes.RunSpeed
 	local abilitySpeedMult = player.PlayerAttributes.AbilitySpeedMult
+	local face = character.Head.face
 	local i = 0
 	
 	abilitySpeedMult.Value = 1.075
 	updateSpeed:FireAllClients(player)
-	
+	face.Texture = "rbxassetid://91743605237089"
 	task.wait(windup)
 
 	while i < linger * 60 do
@@ -310,9 +344,8 @@ function module.doctorMedkit(player, heal, linger, windup)
 		hitboxClone.Touched:Connect(function(hit)
 			local hitboxContacts = hitboxClone:GetTouchingParts()
 			for _, contact in ipairs(hitboxContacts) do
-				if contact.Parent:FindFirstChild("Humanoid") and contact.Parent:FindFirstChild("Humanoid") ~= character:FindFirstChild("Humanoid") and i % 20 == 0 and contact.Parent:FindFirstChild("Players").Team.Value == "Survivor" then
-					local contactPlayer = contact.Parent:FindFirstChild("Players")
-					contactPlayer.Health.Value += heal
+				if contact.Parent:FindFirstChild("Humanoid") and contact.Parent:FindFirstChild("Humanoid") ~= character:FindFirstChild("Humanoid") then
+					contact.Parent:FindFirstChild("Humanoid"):TakeDamage(-heal/20)
 				end
 			end
 		end)
@@ -323,7 +356,7 @@ function module.doctorMedkit(player, heal, linger, windup)
 	
 	abilitySpeedMult.Value = 0.3
 	updateSpeed:FireAllClients(player)
-	
+	face.Texture = "rbxassetid://110621746825104"
 	task.wait(1)
 	
 	abilitySpeedMult.Value = 1
@@ -332,36 +365,32 @@ function module.doctorMedkit(player, heal, linger, windup)
 	abilityReady:FireAllClients(player)
 end
 
-function module.doctorBottle(player, damage, windup, projectileSpeed)
+function module.doctorBottle(player, windup, projectileSpeed)
 	local character = player.Character or player.CharacterAdded:Wait()
 	local humanoid = character:WaitForChild("Humanoid")
 	local walkSpeed = player.PlayerAttributes.WalkSpeed
 	local runSpeed = player.PlayerAttributes.RunSpeed
 	local abilitySpeedMult = player.PlayerAttributes.AbilitySpeedMult
-	local dmg = damage
-	local dmgDealt = false
+	local speedMult = player.PlayerAttributes.SpeedMult
+	local face = character.Head.face
 	local i = 0	
 
 	abilitySpeedMult.Value = 0.6
 	updateSpeed:FireAllClients(player)
+	face.Texture = "rbxassetid://127701447371407"
 
 	task.wait(windup)
+	face.Texture = "rbxassetid://94403218536994"
 
 	character.Weapon.Handle.Transparency = 1
 	local originalCFrame = humanoid.RootPart.CFrame
 
 	while i ~= -1 and i < 180 do
-		if i == 20 then
-			abilitySpeedMult.Value = 1
-			updateSpeed:FireAllClients(player)
-			abilityReady:FireAllClients(player)
-
-		end
 		i += 1
 		local bottleClone = replicatedStorage.Assets["Bottle"]:Clone()
 		bottleClone.Parent = workspace
 		bottleClone.CFrame = originalCFrame
-		bottleClone.CFrame *= CFrame.new(0, 1 + (0.5 * i) - (((i*projectileSpeed))*i)/100, -0.5 - (i*projectileSpeed))
+		bottleClone.CFrame *= CFrame.new(0, 1 + (0.6 * i) - (((i*projectileSpeed))*i)/75, -1 - (i*projectileSpeed))
 		bottleClone.CFrame *= CFrame.Angles(i * -0.4, 0, 0)
 		bottleClone.Transparency = 0
 		bottleClone.Touched:Connect(function(hit)
@@ -369,7 +398,7 @@ function module.doctorBottle(player, damage, windup, projectileSpeed)
 			for _, contact in ipairs(hitboxContacts) do
 				i = -1
 				local j = 0
-				local size = 9
+				local size = 15
 				
 				for j = 1, math.random(3, 6) do
 					local fragmentClone = fragment:Clone()
@@ -401,22 +430,28 @@ function module.doctorBottle(player, damage, windup, projectileSpeed)
 					hitboxClone.Parent = workspace
 					hitboxClone.Shape = "Ball"
 					hitboxClone.Size = Vector3.new(size, size, size)
-					hitboxClone.CFrame = bottleClone.CFrame
+					hitboxClone.CFrame = bottleClone.CFrame + Vector3.new(0, -3/i, 0)
 					hitboxClone.CFrame *= CFrame.Angles(math.rad(90), math.rad(90), 0)
 					heartbeat:Wait()
 					hitboxClone:Destroy()
 					hitboxClone.Touched:Connect(function(hit)
 						local hitboxContacts = hitboxClone:GetTouchingParts()
 						for _, contact in ipairs(hitboxContacts) do
-							if contact.Parent:FindFirstChild("Humanoid") and contact.Parent:FindFirstChild("Players").Team.Value == "Survivor" then
-								local contactPlayer = contact.Parent:FindFirstChild("Players")
-								requestSpeedChange:FireAllClients(contactPlayer, 0.5, 1)
+							if contact.Parent:FindFirstChild("Humanoid") and contact.Parent:FindFirstChild("Humanoid") ~= character:FindFirstChild("Humanoid") then
+								contact.Parent:FindFirstChild("Humanoid"):TakeDamage(-15)
 							end
 						end
 					end)
 				end
 			end
 		end)
+		if i == 35 then
+			face.Texture = "rbxassetid://110621746825104"
+			abilitySpeedMult.Value = 1
+			updateSpeed:FireAllClients(player)
+			abilityReady:FireAllClients(player)
+
+		end
 		heartbeat:Wait()
 		if i ~= -1 then
 			bottleClone:Destroy()
