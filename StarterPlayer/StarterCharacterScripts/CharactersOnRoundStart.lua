065b94a4e3ee5d81292ff6ec8451039dafa653a1
aidan c. <ahcollis2@gmail.com>
2026-06-10 22:34:38 -0400

    CharactersOnRoundStart.lua Model Updates
    Properly enabled players to change appearance upon entering a round based on their class.

--- StarterPlayer/StarterCharacterScripts/CharactersOnRoundStart.lua
+++ StarterPlayer/StarterCharacterScripts/CharactersOnRoundStart.lua
@@ -4,6 +4,8 @@ local player = game.Players.LocalPlayer
 local team = player.PlayerAttributes.Team
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
+local face = character:WaitForChild("Head"):WaitForChild("face")
+local bodyColors = character:WaitForChild("Body Colors")
 
 local stamina = player.PlayerAttributes.Stamina
 local health = player.PlayerAttributes.Health
@@ -21,8 +23,13 @@ local cooldown4max = player.PlayerAttributes.Cooldown4Max
 local equippedSurvivor = player.PlayerAttributes.EquippedCharacterName
 local equippedJuggernaut = player.PlayerAttributes.EquippedJuggernautName
 
-local survivorSpawn = game.Workspace:WaitForChild("SurvivorSpawn")
-local juggernautSpawn = game.Workspace:WaitForChild("JuggernautSpawn")
+local survivorSpawn = game.Workspace:WaitForChild("Main Map"):WaitForChild("SurvivorSpawn")
+local juggernautSpawn = game.Workspace:WaitForChild("Main Map"):WaitForChild("JuggernautSpawn")
+
+character.Weapon.Handle.Transparency = 1
+character.Bow.Handle.Transparency = 1
+character.Hat.Handle.Transparency = 1
+character.Hair.Handle.Transparency = 1
 
 pickTeams.OnClientEvent:Connect(function()
 	if inRound.Value == true then
@@ -37,6 +44,11 @@ pickTeams.OnClientEvent:Connect(function()
 				runSpeed.Value = 32
 				cooldown2max.Value = 20
 				cooldown3max.Value = 24
+				bodyColors.TorsoColor3 = Color3.fromRGB(150, 44, 190)
+				bodyColors.LeftLegColor3 = Color3.fromRGB(60, 14, 110)
+				bodyColors.RightLegColor3 = Color3.fromRGB(60, 14, 110)
+				face.Texture = "rbxassetid://83146247547038"
+				character.Bow.Handle.Transparency = 0
 			elseif equippedSurvivor.Value == "Doctor" then
 				stamina.Value = 100
 				health.Value = 80
@@ -46,6 +58,10 @@ pickTeams.OnClientEvent:Connect(function()
 				runSpeed.Value = 29
 				cooldown2max.Value = 36
 				cooldown3max.Value = 16
+				bodyColors.TorsoColor3 = Color3.fromRGB(122, 222, 90)
+				bodyColors.LeftLegColor3 = Color3.fromRGB(33, 122, 70)
+				bodyColors.RightLegColor3 = Color3.fromRGB(33, 122, 70)
+				face.Texture = "rbxassetid://110621746825104"
 			end
 		elseif team.Value == "Juggernaut" then
 			character:MoveTo(juggernautSpawn.Position + Vector3.new(math.random(-3, 3), 5, math.random(-3, 3)))
@@ -59,6 +75,13 @@ pickTeams.OnClientEvent:Connect(function()
 			cooldown2max.Value = 15
 			cooldown3max.Value = 12
 			cooldown4max.Value = 20
+			bodyColors.TorsoColor3 = Color3.fromRGB(120, 45, 15)
+			bodyColors.LeftLegColor3 = Color3.fromRGB(20, 25, 34)
+			bodyColors.RightLegColor3 = Color3.fromRGB(20, 25, 34)
+			character.Weapon.Handle.Transparency = 0
+			character.Hat.Handle.Transparency = 0
+			character.Hair.Handle.Transparency = 0
+			face.Texture = "rbxassetid://111864759096914"
 		end
 		humanoid.WalkSpeed = walkSpeed.Value
 	end
